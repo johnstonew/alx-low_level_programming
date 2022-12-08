ecd2e2602617406cb216a115a63f405d7367e03f
johnstonew <johnstonewahome@gmail.com>
2022-12-08 13:20:23 -0800

    updated 1-1-quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include<stdlib.h>
+#include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
 int main(void)
 {
-	system("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
