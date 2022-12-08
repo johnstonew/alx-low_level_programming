ae381e18fd7c8e5325db80bc857562275562d928
johnstonew <johnstonewahome@gmail.com>
2022-12-08 10:03:53 -0800

    updated 4-putsfile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 int main () {
 
-	char mystring[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(myString);
+	puts ("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
