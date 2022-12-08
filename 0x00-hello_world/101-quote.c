63dc6446b2822729c62b5f72d044414ff57b0af5
johnstonew <johnstonewahome@gmail.com>
2022-12-08 13:03:06 -0800

    updated 1-1-quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include <unistd.h>
 /*
  * main - Entry point
  * Return: Always 0 (Succ)
  */
 int main(void)
 {
-eprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
