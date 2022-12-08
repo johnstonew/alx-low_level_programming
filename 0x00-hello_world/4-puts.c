f6963b106f5cc43c9ebb34fa6116bb842c297721
johnstonew <johnstonewahome@gmail.com>
2022-12-08 10:16:41 -0800

    updated 4-putsfile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include<stdio.h>
 
-/**
- * Main Function
- *
- * Description: Main starting point of program
- */
-
+/* Description: Main starting point of program */
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-	/**
-	 * return function
-	 *
-	 * Description: return zero
-	 */
-
+	/* Description: return zero */
 	return (0);
 }
