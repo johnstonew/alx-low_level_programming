11639fc3cdf3b4016cc94f777374dc0d8a71f53c
johnstonew <johnstonewahome@gmail.com>
2022-12-08 12:58:27 -0800

    updated 1-1-quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+eprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
