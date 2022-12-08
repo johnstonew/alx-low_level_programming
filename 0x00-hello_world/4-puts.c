fb01c39debdb91381fbe1b7de464a31b81e23c9b
johnstonew <johnstonewahome@gmail.com>
2022-12-08 10:35:45 -0800

    updated 4-putsfile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,12 +7,10 @@
  */
 int main(void)
 {
-	/**
-	 * main - main input of program
-	 *
-	 * Return: returns 0.
-	 */
-
 	puts("\"Programming is like building a multilingual puzzle");
+
+	/**
+	 * return - rentruns 0
+	 */
 	return (0);
 }
