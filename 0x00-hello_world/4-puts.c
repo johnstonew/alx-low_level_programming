aa8d5e3e7e57822bb3a23675c0e12db47736b6fc
johnstonew <johnstonewahome@gmail.com>
2022-12-08 10:25:16 -0800

    updated 4-putsfile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle, followed b a new line.
+ * main: Prints "Programming is like building a multilingual puzzle
  *
  * Return: Always 0.
  *
