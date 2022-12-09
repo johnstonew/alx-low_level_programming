3ef72df393354ee809b26cd03b41b39ceb0132cb
johnstonew <johnstonewahome@gmail.com>
2022-12-09 07:41:28 -0800

    updated 100-print file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int num1, num2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
 
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (digit1 == 8 && digit2 == 9)
+			if (num1 == 8 && num2 == 9)
 				continue;
 
 			putchar(',');
