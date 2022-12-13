ce08b8bf5ea2048392ae6994a4b443e5e9d8b6fc
johnstonew <johnstonewahome@gmail.com>
2022-12-13 09:37:02 -0800

    added 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * print - printts putchar
+ */
+
+void print(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print();
+	return (0);
+}
