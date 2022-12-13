e17447f45b8c6243989e229cecbc7f4ce99a625e
johnstonew <johnstonewahome@gmail.com>
2022-12-13 10:19:18 -0800

    added 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		int ch = 'a';
+
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+		putchar('\n');
+	}
+	x++;
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,3 +24,5 @@ void print(void)
  */
 
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
