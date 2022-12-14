3e2bfc7a8f5529c334e9be5415a20a25666802d8
johnstonew <johnstonewahome@gmail.com>
2022-12-14 08:54:34 -0800

    added comments for betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
-/*
- * _islower: identify if it's lowest number
+/**
+ * _islower - identify if it's lowest number
+ * @c: The character to be inspected
+ *
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
