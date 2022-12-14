e20c134e3e09ab523e68a7e369f16a30f1e01a40
johnstonew <johnstonewahome@gmail.com>
2022-12-14 12:30:36 -0800

    added add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: The character to be added
+ * @b: the character to be added
+ *
+ * Return: return sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
