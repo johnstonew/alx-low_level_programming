288c40230a263ea7c35896d011451ecd52666301
johnstonew <johnstonewahome@gmail.com>
2022-12-14 12:23:18 -0800

    added times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9 times table,
+ *
+ * Return: nothing
+ */
+
+void times_table(void)
+{
+	int a, b, c, u, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (c > 9)
+			{
+				u = c % 10;
+				d = (c - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
