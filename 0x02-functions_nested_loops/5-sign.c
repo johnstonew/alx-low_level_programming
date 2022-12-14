cfe5fc42e2012230d6c29c99f04ffdeb76843da2
johnstonew <johnstonewahome@gmail.com>
2022-12-14 09:08:23 -0800

    added 5 sign file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - identify if it's lowest number
+ * @c: The character to be inspected
+ *
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,3 +19,5 @@ int _islower(int c);
 int _putchar(char c);
 
 int _isalpha(int c);
+
+int print_sign(int n);
