df3cca534773429f1217c8d7c577a3647e876989
johnstonew <johnstonewahome@gmail.com>
2022-12-15 01:27:41 -0800

    updated sum function

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int a, b = 0;
+	int a, sum = 0;
 
 	while (a < 1024)
 	{
 		if ((a % 3 == 0) || (a % 5 == 0))
 		{
-			b++;
+			sum += a;
 		}
 		a++;
 	}
-	printf("%d\n", b);
+	printf("%d\n", sum);
 
 	return (0);
 }
