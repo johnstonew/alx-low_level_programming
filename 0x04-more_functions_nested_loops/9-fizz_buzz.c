b5c910f2a41a296c6950086055448e1ecb38cfb2
johnstonew <johnstonewahome@gmail.com>
2022-12-16 04:59:36 -0800

    added 9-fizz_buzz file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 101; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+
+		printf("%d ", x);
+	}
+	printf("\n");
+}
