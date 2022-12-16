15157558ec03acdd6e0e2a8c4b9ce922c6c535bd
johnstonew <johnstonewahome@gmail.com>
2022-12-16 00:52:52 -0800

    updated print_most_numbers file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,14 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	while (i < 10)
+	for (; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
 
-		_putchar(i + '0');
-		i++;
+		putchar(i + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
