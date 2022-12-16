b6c44687fb7aeab2aff109d056afebfdefcc4612
johnstonew <johnstonewahome@gmail.com>
2022-12-16 03:36:52 -0800

    added comment for n character

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal.
+ * @n: number to be checked
  */
 
 void print_line(int n)
@@ -9,7 +10,7 @@ void print_line(int n)
 	if (n <= 0)
 		putchar('\n');
 
-	for(; n > 0; n--)
+	for (; n > 0; n--)
 	{
 		putchar(95);
 	}
