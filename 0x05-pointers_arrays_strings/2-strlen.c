0de5948733378784bb3bc674532e51c791a575be
johnstonew <johnstonewahome@gmail.com>
2022-12-19 06:57:51 -0800

    added strlen file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to be evaluated
+ *
+ * Return: returns length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
