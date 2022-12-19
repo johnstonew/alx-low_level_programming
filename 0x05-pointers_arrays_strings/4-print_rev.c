fd1a2c4bcbdd283c1946602c426ae117ef2c9a4c
johnstonew <johnstonewahome@gmail.com>
2022-12-19 10:26:00 -0800

    added 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints character to stdout in reverse
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i  > 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
