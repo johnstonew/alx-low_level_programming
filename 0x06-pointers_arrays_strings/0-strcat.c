fe797e22fae8714e5afabd274d0369488faa13de
johnstonew <johnstonewahome@gmail.com>
2022-12-21 08:54:08 -0800

    added while loop

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,14 +14,17 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
+	i = 0;
+	j = 0;
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (src[i] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
