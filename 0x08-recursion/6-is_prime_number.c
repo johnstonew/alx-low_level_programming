ae323dc8c9b34964190d8bec65f320143a547548
johnstonew <johnstonewahome@gmail.com>
2023-01-05 04:58:46 -0800

    removed extra space

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,10 +8,10 @@
 
 int is_prime_number(int n)
 {
-	 if (n <= 1)
-		 return (0);
-	  if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
-		   return (0);
-	  if (n % n == 0)
-		  return (1);
+	if (n <= 1)
+		return (0);
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+		return (0);
+	if (n % n == 0)
+		return (1);
 }
