c344ec87e84e02c5f6ac3ec212eb1dc513e05b86
johnstonew <johnstonewahome@gmail.com>
2023-01-05 04:31:04 -0800

    added 4-pow_recursion.c file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - powers of numbers
+ * @x: integer
+ * @y: integer
+ * Return: an integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
