cad7ef6f47db6a2569d141b520eb8dd029dd4caf
johnstonew <johnstonewahome@gmail.com>
2023-01-09 03:02:30 -0800

    added comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - main program entry
+ * @argc: argument count
+ * @argv: strings in argument
+ * Return: returns 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
