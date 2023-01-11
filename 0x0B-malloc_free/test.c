8caa585c40fa374ab4d69fada8c239c76db038c7
johnstonew <johnstonewahome@gmail.com>
2023-01-11 07:46:28 -0800

    added 3-alloc_grid.c file

--- 0x0B-malloc_free/test.c
+++ 0x0B-malloc_free/test.c
@@ -1,19 +1,44 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+nt **grid, int width, int height)
+{
+	    int w;
+	        int h;
+		
+		    h = 0;
+		        while (h < height)
+				    {
+					            w = 0;
+						            while (w < width)
+								            {
+										                printf("%d ", grid[h][w]);
+												            w++;
+													            }
+							            printf("\n");
+								            h++;
+									        }   
+}
 
+/**
+ *  * main - check the code for ALX School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-	char *s;
+	    int **grid;
+	    
+	        grid = alloc_grid(6, 4);
+		    if (grid == NULL)
+			        {
+					        return (1);
+						    }
+		        print_grid(grid, 6, 4);
+			    printf("\n");
+			        grid[0][3] = 98;
+				    grid[3][4] = 402;
+				        print_grid(grid, 6, 4);
+					    return (0);
+}int main(void)
 
-	s = str_concat("Betty ", "Holberton");
-
-	if (s == NULL)
-	{
-		printf("failed\n");
-		return (1);
-	}
-	 printf("%s\n", s);
-	 free(s);
-	 return (0);
-}
