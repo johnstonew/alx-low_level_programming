88f2def26b20e5489a70c16f3ff88307343dddad
johnstonew <johnstonewahome@gmail.com>
2023-01-11 07:23:53 -0800

    added 2-str_concat.c file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenate two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: pointer to a new string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *newstr;
+	unsigned int i, j, k, limit;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	newstr = malloc(sizeof(char) * (i + j + 1));
+
+	if (newstr ==NULL)
+	{
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+		newstr[k] = s1[k];
+
+	limit = j;
+
+	for (j = 0; j <= limit; k++, j++)
+	{
+		newstr[k] = s2[j];
+	}
+
+	return (newstr);
+}
--- 0x0B-malloc_free/test.c
+++ 0x0B-malloc_free/test.c
@@ -6,12 +6,13 @@ int main(void)
 {
 	char *s;
 
-	s = _strdup("First, solve the problem. Then, write the code.");
-	 if (s == NULL)
-	 {
-		 printf("failed to allocate memory\n");
-		 return (1);
-	 }
+	s = str_concat("Betty ", "Holberton");
+
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
 	 printf("%s\n", s);
 	 free(s);
 	 return (0);
