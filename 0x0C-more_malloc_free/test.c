edea1244052669a0cbf13c51aebb3535dccae76f
johnstonew <johnstonewahome@gmail.com>
2023-01-13 09:01:07 -0800

    added 3-array_range.c file

--- 0x0C-more_malloc_free/test.c
+++ 0x0C-more_malloc_free/test.c
@@ -3,24 +3,29 @@
 #include <stdlib.h>
 #include <limits.h>
 
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		 if (i % 10)
+			  printf(" ");
+		 if (!(i % 10) && i)
+			  printf("\n");
+		 printf("0x%02x", buffer[i]);
+		 i++;
+	}
+	printf("\n");
+}
+
 int main(void)
 {
-	char *c;
-	int *i;
-	float *f;
-	double *d;
+	int *a;
 
-	c = malloc_checked(sizeof(char) * 1024);
-	printf("%p\n", (void *)c);
-	i = malloc_checked(sizeof(int) * 402);
-	printf("%p\n", (void *)i);
-	f = malloc_checked(sizeof(float) * 100000000);
-	printf("%p\n", (void *)f);
-	d = malloc_checked(INT_MAX);
-	printf("%p\n", (void *)d);
-	free(c);
-	free(i);
-	free(f);
-	free(d);
+	a = array_range(0, 10);
+	simple_print_buffer(a, 11);
+	free(a);
 	return (0);
 }
