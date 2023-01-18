5d93948c82957cca8334f0b63d7e83e64a358437
johnstonew <johnstonewahome@gmail.com>
2023-01-18 11:12:33 -0800

    removed comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,11 +19,7 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
-
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif
