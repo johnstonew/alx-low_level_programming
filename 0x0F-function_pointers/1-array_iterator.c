85bf22972cf2001b6a8b4da3a61e592848a3812c
johnstonew <johnstonewahome@gmail.com>
2023-01-19 04:38:53 -0800

    check if array is null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (i = 0; i < (int) size; i++)
 	{
 		action(array[i]);
