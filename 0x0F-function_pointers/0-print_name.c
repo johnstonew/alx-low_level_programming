f90a83e56bae47fc6a689792822ac1a603c92ae6
johnstonew <johnstonewahome@gmail.com>
2023-01-19 04:08:07 -0800

    added print name file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: pointer to function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void print_name(char *name, void (*f)(char *));
+
+#endif;
