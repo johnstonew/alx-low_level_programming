cc2c817b037756ada9cd859347f24ce0ab612445
johnstonew <johnstonewahome@gmail.com>
2023-01-22 08:02:27 -0800

    added strings file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings
+ * @separator: eparator
+ * @n: number
+ * @...: The list of numbers
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *p;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL && i != 0)
+			printf("%s", separator);
+		p = va_arg(valist, char *);
+		printf("%s", (p == NULL) ? "(nil)" : p);
+	}
+	printf("\n");
+	va_end(valist);
+}
