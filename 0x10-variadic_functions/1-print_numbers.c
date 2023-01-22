f25a7a39fa334845e8ba83e6b47344bb37300ff7
johnstonew <johnstonewahome@gmail.com>
2023-01-22 07:57:32 -0800

    added print numbers file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: eparator
+ * @n: number of elements
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+
+	}
+	putchar('\n');
+	va_end(ap);
+}
