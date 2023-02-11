1c53c1026d4119a203f77dca826557a2eb034ebe
johnstonew <johnstonewahome@gmail.com>
2023-02-11 06:58:43 -0800

    created append to text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
