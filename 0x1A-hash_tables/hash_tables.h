f1b4fe6563c29f5e2ba2852a9846171d717f7a80
Johnstone Wahome <johnstonewahome@gmail.com>
2023-04-07 20:47:16 +0300

    added func to header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -28,3 +28,4 @@ typedef struct hash_table_s
 } hash_table_t;
 
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
